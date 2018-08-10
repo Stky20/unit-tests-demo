9a9689d516ef245631fa85cc5bc7d6586030f20e
Rostislav Vatolin <vatolinrp@icloud.com>
2018-08-10 08:00:51 +0300

    unit tests added

---
+++ src/test/java/com/epam/UserServiceDaoImplTest.java
@@ -0,0 +1,48 @@
+package com.epam;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class UserServiceDaoImplTest {
+
+  private static UserService userService;
+  private static UserDao userDao;
+
+  @BeforeAll
+  public static void setup() {
+    userDao = Mockito.mock(UserDao.class);
+    userService = new UserServiceDaoImpl(userDao);
+  }
+
+  @AfterEach
+  public void reset() {
+    Mockito.reset(userDao);
+  }
+
+  @Test
+  public void exceptionFlow() {
+    final String errorMessage = "unknown error";
+    Mockito.doThrow(new RuntimeException(errorMessage)).when(userDao).deleteUser(1L);
+    try {
+      userService.deleteUserById(1L);
+      Assertions.fail("expected exception");
+    } catch ( IllegalArgumentException e ) {
+      Assertions.assertEquals(errorMessage, e.getMessage());
+    }
+  }
+
+  @Test
+  public void successfulFlow() {
+    userService.deleteUserById(1L);
+    Mockito.verify(userDao).deleteUser(1L);
+  }
+
+  @Test
+  public void invalidFlow() {
+    userService.deleteUserById(0L);
+    Mockito.verifyZeroInteractions(userDao);
+  }
+}
